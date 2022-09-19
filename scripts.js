1dcc33202a6d1f0ed9a4153e0a4da34856630f06
Abdelkrim <krimouv1995@gmail.com>
2022-09-20 00:08:53 +0100

    Some tries

--- scripts.js
+++ scripts.js
@@ -1,12 +1,32 @@
 const container = document.querySelector(".grid-container");
 
 for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
+    var row = document.createElement("div");
     row.className = "row";
     for (let j = 0; j < 16; j++) {
-        const column = document.createElement("div");
+        var column = document.createElement("div");
         column.className = "column";
         row.appendChild(column);
     }
     container.appendChild(row);
 }
+
+let divs = document.querySelectorAll(".column");
+
+
+// divs.forEach(div => {
+//     var mouseState = false;
+//     function mouseDown(e) {
+//         mouseState = true;
+//     }
+//     function mouseMove(e) {
+//         if (!mouseState) return;
+//         div.style.backgroundColor = "red";
+//     }
+//     function mouseUp(e) {
+//         mouseState = false;
+//     }
+//     div.addEventListener("mousedown", mouseDown);
+//     div.addEventListener("mouseover", mouseMove);
+//     div.addEventListener("mouseup", mouseUp);
+// });
